4db2bf4fd6f6c1f2689160ddfaf46c394830cea4
saumya-banthia <29239098+saumya-banthia@users.noreply.github.com>
2023-05-29 13:57:58 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Volume Boost
+
+A Decky plugin to boost default audio device volume to greater than 100% (upto 150%).
+
+## Pre-requisites
+
+pactl service installed **[Note: pactl (Pulse Audio Control) is installed by default in your Deck.]**
+
+### Known bug (or feature?)
+
+* Currently you can only set the volume above 100% using the UI of the plugin. If the volume is above 100% and  you use the volume buttons to increase or decrease the volume, the volume jumps back to the default range (0-100). Think of this more as a feature than a bug, as it allows you to get back to the normal function range with the press of a button.
---
+++ src/index.tsx
@@ -0,0 +1,69 @@
+import {
+  definePlugin,
+  PanelSection,
+  PanelSectionRow,
+  ServerAPI,
+  staticClasses,
+  SliderField,
+} from "decky-frontend-lib";
+import { VFC,
+         useState 
+        } from "react";
+import { IoMdMegaphone } from "react-icons/io";
+import { FaVolumeUp } from 'react-icons/fa';
+
+const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
+  const [volumeState, setVolumeState] = useState<number>(0);
+
+  const getDefaultSinkVolume = async () => {
+    const data = await serverAPI.callPluginMethod<{}, number>("get_volume_state", {});
+    if (data.success) {
+      setVolumeState(data.result);
+    }
+  }
+  getDefaultSinkVolume();
+  
+  const setDefaultSinkVolume = async(sliderValue: number) => {
+    const value = Math.min(Math.max(sliderValue, 0), 150);
+    console.log("Setting volume to: " + value);
+    const data = await serverAPI.callPluginMethod<{ value: number }, boolean>("set_volume_state", { value })
+    if (data.success) {
+      getDefaultSinkVolume();
+    }
+  }
+
+  return (
+    <PanelSection title="Settings">
+        <PanelSectionRow>
+          <SliderField
+            value={volumeState}
+            min={0}
+            max={150}
+            step={1}
+            label='Volume'
+            description='Increases volume bounds of Default sound device to 150%'
+            editableValue
+            icon={<FaVolumeUp />}
+            onChange={setDefaultSinkVolume}
+          />
+        </PanelSectionRow>
+        <PanelSectionRow>
+          <div>
+            Note: It's recommended to not increase volume above 100%, 
+            as it may cause distortion and damage to your speakers. 
+            If you do increase the volume above 100% temporarily, 
+            it's recommended to restore the volume to or below 100% when not in use. 
+            Use at your own risk.
+          </div>
+        </PanelSectionRow>
+    </PanelSection>
+  );
+};
+
+export default definePlugin((serverApi: ServerAPI) => {
+  return {
+    title: <div className={staticClasses.Title}>Volume Boost</div>,
+    content: <Content serverAPI={serverApi} />,
+    icon: <IoMdMegaphone />,
+  };
+});
